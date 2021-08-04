8884e6a5032a02944fd468e44b753aff8a6d501a
Dave MacPherson <davemacpherson@gmail.com>
2021-08-03 22:15:20 -0400

    Update ep depth charts.py
    Added Seattle

--- ep depth charts.py
+++ ep depth charts.py
@@ -6,13 +6,13 @@ import time
 from random import randint
 
 # List of all teams to be passed to the URL
-team_list = ["1580/anaheim-ducks","72/arizona-coyotes","52/boston-bruins","53/buffalo-sabres","54/calgary-flames","55/carolina-hurricanes","56/chicago-blackhawks","57/colorado-avalanche","58/columbus-blue-jackets","59/dallas-stars","60/detroit-red-wings","61/edmonton-oilers","62/florida-panthers","79/los-angeles-kings","63/minnesota-wild","64/montreal-canadiens","65/nashville-predators","66/new-jersey-devils","67/new-york-islanders","68/new-york-rangers","69/ottawa-senators","70/philadelphia-flyers","71/pittsburgh-penguins","73/san-jose-sharks","74/st.-louis-blues","75/tampa-bay-lightning","76/toronto-maple-leafs","77/vancouver-canucks","22211/vegas-golden-knights","78/washington-capitals","9966/winnipeg-jets"]
+team_list = ["1580/anaheim-ducks","72/arizona-coyotes","52/boston-bruins","53/buffalo-sabres","54/calgary-flames","55/carolina-hurricanes","56/chicago-blackhawks","57/colorado-avalanche","58/columbus-blue-jackets","59/dallas-stars","60/detroit-red-wings","61/edmonton-oilers","62/florida-panthers","79/los-angeles-kings","63/minnesota-wild","64/montreal-canadiens","65/nashville-predators","66/new-jersey-devils","67/new-york-islanders","68/new-york-rangers","69/ottawa-senators","70/philadelphia-flyers","71/pittsburgh-penguins","73/san-jose-sharks","74/st.-louis-blues","75/tampa-bay-lightning","76/toronto-maple-leafs","77/vancouver-canucks","22211/vegas-golden-knights","78/washington-capitals","9966/winnipeg-jets", "27336/seattle-kraken"]
 # List of shortened team names to use for output
-team_list_short = ["ANA","ARI","BOS","BUF","CGY","CAR","CHI","COL","CBJ","DAL","DET","EDM","FLA","LAK","MIN","MTL","NSH","NJD","NYI","NYR","OTT","PHI","PIT","SJS","STL","TBL","TOR","VAN","VGK","WSH","WPG"]
+team_list_short = ["ANA","ARI","BOS","BUF","CGY","CAR","CHI","COL","CBJ","DAL","DET","EDM","FLA","LAK","MIN","MTL","NSH","NJD","NYI","NYR","OTT","PHI","PIT","SJS","STL","TBL","TOR","VAN","VGK","WSH","WPG","SEA"]
 
 # Initialize and loop through the team list
 team_no = 0
-while team_no < 31:
+while team_no < 32:
     # Select the team URL and shortened name from the two lists
     team_id = team_list[team_no]
     team_short = team_list_short[team_no]
